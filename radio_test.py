979fe537bd7e2e8e0606b3438b2a9457aca46994
Unknown <neil@netaccess.me.uk>
2018-08-27 16:39:43 +0100

    updated radio_test
    extended radio_test to receive packets in promiscuous node as per http://www.kittley.com/2018/04/06/blog-async-rfm69/
    needs to be test on board

--- radio_test.py
+++ radio_test.py
@@ -7,6 +7,22 @@ node_id = 1
 network_id = 100
 recipient_id = 2
 
-with Radio(RF69_868MHZ, node_id, network_id, isHighPower=True, verbose=True) as radio:
+with Radio(RF69_868MHZ, node_id, network_id, isHighPower=True, verbose=True, promiscuousMode=True) as radio:
     print(radio.read_temperature())
     print(radio.read_registers())
+    rx_counter = 0
+
+    while True:
+
+        # Every 10 seconds get packets
+        if rx_counter > 10:
+            rx_counter = 0
+
+            # Process packets
+            for packet in radio.get_packets():
+                print(packet)
+
+        print("Listening...", len(radio.packets), radio.mode_name)
+        delay = 0.5
+        rx_counter += delay
+        time.sleep(delay)
